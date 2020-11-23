crdroidandroid/android_packages_apps_crDroidSettings

Quick settings: Add Haptic Feedback to tiles [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/QuickSettings.java
@@ -63,6 +63,8 @@ public class QuickSettings extends SettingsPreferenceFragment {
                 LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT);
         LineageSettings.Secure.putIntForUser(resolver,
                 LineageSettings.Secure.QS_SHOW_AUTO_BRIGHTNESS, 1, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.QUICK_SETTINGS_VIBRATE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override

AOSAP/platform_packages_apps_AndromedaSettings

Signed-off-by: mracar07 <umutcan@umutcanacar.me>
Change-Id: I268235114674edd9562bd8589e06517e2757c076

src/com/aosap/settings/fragments/QuickSettings.java
@@ -50,6 +50,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = mContext.getContentResolver();
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TILE_TITLE_VISIBILITY, 1, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.System.QUICK_SETTINGS_VIBRATE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override