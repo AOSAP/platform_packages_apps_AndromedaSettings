020f6c6c88d40be6cc8dc0415d83c944f5e22f2c
Pranav Vashi <neobuddy89@gmail.com>
2021-06-29 10:25:32 +0530

    Alert Slider: Add toggle to disable notifications [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/aosap/settings/fragments/NotificationSettings.java
+++ src/com/aosap/settings/fragments/NotificationSettings.java
@@ -7,13 +7,41 @@ import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
+import android.content.Context;
+import android.content.res.Resources;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
+
 public class NotificationSettings extends SettingsPreferenceFragment {
+
+    private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
+
+    private Preference mAlertSlider;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.aosap_settings_notifications);
+
+        final Context mContext = getActivity().getApplicationContext();
+
+        final Resources res = mContext.getResources();
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mAlertSlider = (Preference) findPreference(ALERT_SLIDER_PREF);
+        boolean mAlertSliderAvailable = res.getBoolean(
+                com.android.internal.R.bool.config_hasAlertSlider);
+        if (!mAlertSliderAvailable)
+            prefSet.removePreference(mAlertSlider);
     }
 
     @Override
